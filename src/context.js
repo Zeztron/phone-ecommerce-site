954fabf5403e449b8043974320c8784dcb19c70a
Zeztron <pateltharsh@gmail.com>
2019-01-31 14:06:51 -0500

    added removeitem from cart method

--- src/context.js
+++ src/context.js
@@ -81,11 +81,36 @@ class ProductProvider extends Component {
   }
 
   removeItem = (id) => {
-    console.log("Item removed");
+    // console.log("Item removed");
+    let tempProducts = [...this.state.products];
+    let tempCart = [...this.state.cart];
+    tempCart = tempCart.filter(item => item.id !== id);
+    const index = tempProducts.indexOf(this.getItem(id));
+    let removedProduct = tempProducts[index];
+    removedProduct.inCart = false;
+    removedProduct.count = 0;
+    removedProduct.total = 0;
+
+    this.setState(() => {
+      return {
+        cart: [...tempCart],
+        product: [...tempProducts]
+      }
+    }, () => {
+      this.addTotals();
+    })
   }
 
   clearCart = () => {
-    console.log("cart cleared");
+    // console.log("cart cleared");
+    this.setState(() => {
+      return {
+        cart: []
+      }
+    }, () => {
+      this.setProducts();
+      this.addTotals();
+    });
   }
 
   addTotals = () => {
